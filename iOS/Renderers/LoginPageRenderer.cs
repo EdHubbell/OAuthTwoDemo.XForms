bd61b666ee1ac94f73860f7b9ee6ccaebe981581
Ed Hubbell <edhubbell@gmail.com>
2014-10-19 13:32:11 -0400

    not sure what I changed

--- iOS/Renderers/LoginPageRenderer.cs
+++ iOS/Renderers/LoginPageRenderer.cs
@@ -36,7 +36,8 @@ namespace OAuthTwoDemo.XForms.iOS
 
 
 					if (eventArgs.IsAuthenticated) {
-
+						// eventArgs	{Xamarin.Auth.AuthenticatorCompletedEventArgs}	Xamarin.Auth.AuthenticatorCompletedEventArgs
+						//Account	{__username__=&token=%7B%22user%22%3A%7B%22userId%22%3A%22Aad%3AyX1sEIBPqlE6yz098wFy_eZdzI618W1BLFzIiiwQFhI%22%7D%2C%22authenticationToken%22%3A%22eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjAifQ.eyJleHAiOjE0MTYzMjg3NjIsImlzcyI6InVybjptaWNyb3NvZnQ6d2luZG93cy1henVyZTp6dW1vIiwidmVyIjoyLCJhdWQiOiJBYWQiLCJ1aWQiOiJBYWQ6eVgxc0VJQlBxbEU2eXowOTh3RnlfZVpkekk2MThXMUJMRnpJaWl3UUZoSSIsInVybjptaWNyb3NvZnQ6Y3JlZGVudGlhbHMiOiJra0oyOEJIMDRQMkR2QkdqQkRJUDZKKzRxWTdwdkJQN1VLcndMRG9VdHR4NTlwa2ZkVlM3L3VWWHpERGlaczVZeS96b3hacGorckhWSFNlN2dXVFU0dDAvK25EZk1hNkxJbmF2QStVWlZyOWszeWwxaC8wb2M5MS9idkFSdkZBTkNWcW5vTFlHQlB0c0ZYVmd3WUVJYkE9PSJ9.C38kB3u9Bipa4Y-KW4hACe9COlRR-kKx3OC_PGI5eGE%22%7D}	Xamarin.Auth.Account
 						// Use eventArgs.Account to do wonderful things
 						App.Instance.SaveToken (eventArgs.Account.Properties ["token"]);
 					} else {
